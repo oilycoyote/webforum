90830d2900653db75e4176314cfecf1d3bfb8032
oilycoyote <juan.aguayo@aguayo.co>
2020-04-22 01:30:52 -0500

    Reply function is now working.

--- boards/urls.py
+++ boards/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('topic/<int:board_id>', views.topics, name='topics'),
     path('topic/<int:board_id>/new_topic', views.new_topic, name='new_topic'),
     path('post/<int:topic_id>', views.post, name='post'),
-    path('reply_post/', views.reply_post, name='reply_post')
+    path('post/<int:topic_id>/reply_post', views.reply_post, name='reply_post')
 ]
--- boards/views.py
+++ boards/views.py
@@ -17,11 +17,11 @@ def index(request):
         board.num_topics= len(Topic.objects.all().filter(board=board))
 
 
-    # Get total posts in a board
+    # Gets total posts in a board
     for board in boards_list:
         board.total_posts = len(Post.objects.all().filter(board=board))
 
-    # Get last updated post
+    # Gets last updated post
     for board in boards_list:
         board.post_last_modified = Post.objects.all().filter(board=board).order_by('-updated_at').first()
      
@@ -37,17 +37,17 @@ def index(request):
 # Topic's Page
 def topics(request, board_id):
 
-    # Get board by URL's Id. (board_id passed via URL)
+    # Gets board by URL's Id. (board_id passed via URL)
     board = get_object_or_404(Board, pk=board_id)
 
-    # Get topics per board
+    # Gets topics per board
     topics = Topic.objects.all().filter(board=board)
 
-    # Get last updated post
+    # Gets last updated post
     for topic in topics:
         topic.post_last_modified = Post.objects.all().filter(topic=topic).order_by('-updated_at').first()
 
-    # Get posts per topic    
+    # Gets posts per topic    
     for topic in topics:
         topic.posts_published = len(topic.posts.all())
 
@@ -62,31 +62,39 @@ def topics(request, board_id):
 
     return render(request,'boards/topics.html', context)
 
+# Post new topic
 @login_required
 def new_topic(request, board_id):
     
+    # Gets Board to relate topic and post to specific board
     board = get_object_or_404(Board, pk=board_id)
     
+    # Posting a new topic and post (Method: POST) 
     if request.method == 'POST':
         topic_form = TopicNewForm(request.POST)
         post_form = PostNewForm(request.POST)
 
         if topic_form.is_valid() and post_form.is_valid():
+            
+            # Saves data to topic model (new instance of topic) 
             my_topic = topic_form.save(commit=False)
             my_topic.board = Board.objects.all().filter(id=board_id).first()
             my_topic.starter = request.user
-            my_topic.save()            
+            my_topic.save()
 
+            # Saves data to post model (new instance of post) The first post required to create a new topic         
             my_post = post_form.save(commit=False)
             my_post.topic = Topic.objects.all().filter(id=my_topic.id).first()
             my_post.board = Board.objects.all().filter(id=board_id).first()
             my_post.created_by = request.user
             my_post.save()
             
+            # Success message and redirect to post created.
             topic_subject = topic_form.cleaned_data.get('subject')
             messages.success(request, f'Subject posted: {topic_subject}')
             return redirect('post', topic_id=my_topic.id)
     
+    # Gets form and sends to template.
     else:        
         topic_form = TopicNewForm()
         post_form = PostNewForm()
@@ -122,5 +130,20 @@ def post(request, topic_id):
     return render(request,'boards/post.html', context)
 
 
-def reply_post(request):
-    return render(request,'boards/reply_post.html')
+def reply_post(request, topic_id):
+
+    topic_of_posts = get_object_or_404(Topic, pk=topic_id)
+    related_posts = topic_of_posts.posts.all().order_by('-created_at')
+
+    context = {
+        'related_posts' : related_posts,
+        'topic_of_posts' : topic_of_posts,
+
+    }
+
+    print(related_posts)
+    print(topic_of_posts.board)
+    print(topic_of_posts.subject)
+
+
+    return render(request,'boards/reply_post.html', context)
